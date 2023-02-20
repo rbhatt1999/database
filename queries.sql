bf1a627dad0b9b5c91b72556761e17f290b91e30
rbhatt199924 <rbhatt199924@gmail.com>
2023-02-20 10:54:07 +0530

    update select all to select name for test 2 and 3

--- queries.sql
+++ queries.sql
@@ -1,8 +1,8 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name LIKE '%mon';
-SELECT * from animals WHERE date_of_birth BETWEEN '2016/01/01' and '2019/12/31';
-SELECT * from animals WHERE neutered = TRUE and escape_attempts < 3;
+SELECT name from animals WHERE date_of_birth BETWEEN '2016/01/01' and '2019/12/31';
+SELECT name from animals WHERE neutered = TRUE and escape_attempts < 3;
 SELECT date_of_birth from animals WHERE name = 'Agumon' OR name =  'Pikachu' ;
 SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
 SELECT * from animals WHERE neutered = TRUE;
