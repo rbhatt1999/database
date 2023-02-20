a3aa57abd63e43d9e2a2901e3a4f0a4952f62620
rbhatt199924 <rbhatt199924@gmail.com>
2023-02-20 10:49:58 +0530

    add database point into sql files

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
+CREATE DATABASE vet_clinic;
 CREATE TABLE animals (
-    name varchar(100)
-);
+    id SERIAL PRIMARY KEY,
+    name varchar(100) Not NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL(10,2));
